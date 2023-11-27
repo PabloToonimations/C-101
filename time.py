a7f4cba60571c908c8255b17c29af3654deda949
PabloToonimations <79716578+PabloToonimations@users.noreply.github.com>
2023-11-27 17:53:10 -0600

    Add files via upload

---
+++ time.py
@@ -0,0 +1,18 @@
+import time
+
+def countdown_timer(seconds):
+
+    while seconds > 0:
+
+        mins = int(seconds / 60)
+        secs = int(seconds % 60)
+
+        timer = f'{mins}:{secs}' 
+
+        print(timer) 
+
+        seconds -= 1 
+
+    print('¡Se acabó el tiempo!') 
+# input time in seconds 
+seconds = input("Ingresa el tiempo en segundos: ")
